2e54335407d4b9ac9590574a859341c2f3469c41
Nick Phillips <nwp@zepler.net>
2020-03-30 14:30:23 +1300

    Remove elpa dir from emacs.d install

--- emacs.d/elpa/sass-mode-3.0.16/sass-mode-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; sass-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("sass-mode.el") (21378 41669 168124 203000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; sass-mode-autoloads.el ends here
--- emacs.d/elpa/yaml-mode-0.0.9/yaml-mode-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; yaml-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("yaml-mode.el") (21378 41663 414881 966000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; yaml-mode-autoloads.el ends here
